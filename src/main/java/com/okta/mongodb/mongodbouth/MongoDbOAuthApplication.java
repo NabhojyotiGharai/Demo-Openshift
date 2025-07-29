9399bd32bbdec8aab3b4497323f49a25dc7951a6
Nabhojyoti Gharai <nabhojoti.gharai@gmail.com>
2025-07-29 11:26:59 +0530

    changing files

--- src/main/java/com/okta/mongodb/mongodbouth/MongoDbOAuthApplication.java
+++ src/main/java/com/okta/mongodb/mongodbouth/MongoDbOAuthApplication.java
@@ -2,6 +2,9 @@ package com.okta.mongodb.mongodbouth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
